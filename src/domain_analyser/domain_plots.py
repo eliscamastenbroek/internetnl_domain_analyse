8409d2ac2589845fd1e075a750c503bcc2edf429
EVLT <>
2022-01-28 17:48:40 +0100

    stem goed gezet

--- src/domain_analyser/domain_plots.py
+++ src/domain_analyser/domain_plots.py
@@ -886,7 +886,7 @@ def make_verdeling_per_aantal_categorie(categories, image_directory,
     fig.savefig(im_file)
 
     if export_svg:
-        svg_image_file = hc_dir / Path(im_file.with_suffix(".svg").stem)
+        svg_image_file = hc_dir / Path(im_file.stem).with_suffix(".svg")
         _logger.info(f"Saving plot to {svg_image_file}")
         fig.savefig(svg_image_file)
 
