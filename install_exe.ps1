6eb27c9dcb172494369023e00183e90375db2fc4
EVLT <EVLT@cbs.nl>
2024-03-25 20:05:06 +0100

    removed install script

--- install_exe.ps1
+++
@@ -1,7 +0,0 @@
-$TOOLS_DIR="\\cbsp.nl\productie\primair\WTE\Werk\WTE_Python\Python3.9"
-$cmd="pip install . --no-build-isolation --prefix $TOOLS_DIR -U"
-$cmd = $cmd -replace '\s+', ' '
-Write-Output $cmd
-Invoke-Expression $cmd
-
-
